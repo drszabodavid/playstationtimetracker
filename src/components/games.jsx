89f514b4fce5d74672c663dd52038973e17185d9
Dávid Szabó <drszabodavid@gmail.com>
2019-12-03 16:13:53 +0100

    game can be send to backend /and be saved/

--- src/components/games.jsx
+++ src/components/games.jsx
@@ -1,144 +1,145 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
-import { paginate } from "../utils/paginate";
+import {paginate} from "../utils/paginate";
 import GamesTable from "./gamesTable";
 import _ from "lodash";
 import "./css/games.css";
-import { getGames } from "../services/fakeGameService";
+import {getGames} from "../services/fakeGameService";
+import axios from "axios";
 
 class Games extends Component {
-  state = {
-    games: [],
-    types: ["Completed", "Starred", "All Games"],
-    pageSize: 4,
-    currentPage: 1,
-    selectedType: "",
-    sortColumn: { path: "title", order: "asc" }
-  };
+    state = {
+        games: [],
+        types: ["Completed", "Starred", "All Games"],
+        pageSize: 4,
+        currentPage: 1,
+        selectedType: "",
+        sortColumn: {path: "title", order: "asc"}
+    };
 
-  componentDidMount() {
-    this.setState({
-      games: getGames()
-    });
-  }
-
-  handleDelete = game => {
-    const games = this.state.games.filter(g => g.id !== game.id);
-    this.setState({ games: games });
-  };
-
-  handleComplete = game => {
-    const games = [...this.state.games];
-    const index = games.indexOf(game);
-    games[index] = { ...games[index] };
-    games[index].completed = !games[index].completed;
-    games[index].liked = !games[index].liked;
-    this.setState({ games });
-  };
-
-  handleLike = game => {
-    const games = [...this.state.games];
-    const index = games.indexOf(game);
-    games[index] = { ...games[index] };
-    games[index].liked = !games[index].liked;
-    this.setState({ games });
-  };
-
-  handlePageChange = page => {
-    this.setState({ currentPage: page });
-  };
-
-  handleGenreSelect = type => {
-    this.setState({ selectedType: type, currentPage: 1 });
-  };
-
-  handleSort = sortColumn => {
-    this.setState({ sortColumn });
-  };
-
-  filterGames = selectedType => {
-    if (selectedType === "Completed") {
-      return this.state.games.filter(game => game.completed === true);
+    async componentDidMount() {
+        const response = await getGames(2);
+        console.log(response.data)
+        this.setState({games: response.data})
     }
-    if (selectedType === "Starred") {
-      return this.state.games.filter(game => game.liked === true);
+
+    handleDelete = game => {
+        const games = this.state.games.filter(g => g.id !== game.id);
+        this.setState({games: games});
+    };
+
+    handleComplete = game => {
+        const games = [...this.state.games];
+        const index = games.indexOf(game);
+        games[index] = {...games[index]};
+        games[index].completed = !games[index].completed;
+        games[index].liked = !games[index].liked;
+        this.setState({games});
+    };
+
+    handleLike = game => {
+        const games = [...this.state.games];
+        const index = games.indexOf(game);
+        games[index] = {...games[index]};
+        games[index].liked = !games[index].liked;
+        this.setState({games});
+    };
+
+    handlePageChange = page => {
+        this.setState({currentPage: page});
+    };
+
+    handleGenreSelect = type => {
+        this.setState({selectedType: type, currentPage: 1});
+    };
+
+    handleSort = sortColumn => {
+        this.setState({sortColumn});
+    };
+
+    filterGames = selectedType => {
+        if (selectedType === "Completed") {
+            return this.state.games.filter(game => game.completed === true);
+        }
+        if (selectedType === "Starred") {
+            return this.state.games.filter(game => game.liked === true);
+        }
+        return this.state.games;
+    };
+
+    getPagedData = () => {
+        const {
+            pageSize,
+            currentPage,
+            games: allgames,
+            selectedType,
+            sortColumn
+        } = this.state;
+
+        //filter data
+        const filtered = this.filterGames(selectedType);
+
+        // sort data
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        // paginate data
+        const games = paginate(sorted, currentPage, pageSize);
+
+        return {totalCount: filtered.length, data: games};
+    };
+
+    handleRecord = () => {
+        this.setState({
+            games: getGames()
+        });
+    };
+
+    render() {
+        const {length: count} = this.state.games;
+        const {pageSize, currentPage, sortColumn} = this.state;
+
+        if (count === 0)
+            return (
+                <p className="under-header-text">There are no games in the database</p>
+            );
+
+        const {totalCount, data: games} = this.getPagedData();
+
+        return (
+            <div className="row">
+                <div className="col-2">
+                    <ListGroup
+                        onItemSelect={this.handleGenreSelect}
+                        items={this.state.types}
+                        selectedItem={this.state.selectedType}
+                    />
+                </div>
+                <div className="col">
+                    <p className="under-header-text">
+                        Showing {totalCount} games in the database.
+                    </p>
+
+                    <GamesTable
+                        handleRecord={this.handleRecord}
+                        sortColumn={sortColumn}
+                        games={games}
+                        onLike={this.handleLike}
+                        onDelete={this.handleDelete}
+                        onSort={this.handleSort}
+                        onComplete={this.handleComplete}
+                    />
+
+                    <Pagination
+                        onPageChange={this.handlePageChange}
+                        itemsCount={totalCount}
+                        pageSize={pageSize}
+                        currentPage={currentPage}
+                    />
+                </div>
+            </div>
+        );
     }
-    return this.state.games;
-  };
-
-  getPagedData = () => {
-    const {
-      pageSize,
-      currentPage,
-      games: allgames,
-      selectedType,
-      sortColumn
-    } = this.state;
-
-    //filter data
-    const filtered = this.filterGames(selectedType);
-
-    // sort data
-    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
-
-    // paginate data
-    const games = paginate(sorted, currentPage, pageSize);
-
-    return { totalCount: filtered.length, data: games };
-  };
-
-  handleRecord = () => {
-    this.setState({
-      games: getGames()
-    });
-  };
-
-  render() {
-    const { length: count } = this.state.games;
-    const { pageSize, currentPage, sortColumn } = this.state;
-
-    if (count === 0)
-      return (
-        <p className="under-header-text">There are no games in the database</p>
-      );
-
-    const { totalCount, data: games } = this.getPagedData();
-
-    return (
-      <div className="row">
-        <div className="col-2">
-          <ListGroup
-            onItemSelect={this.handleGenreSelect}
-            items={this.state.types}
-            selectedItem={this.state.selectedType}
-          />
-        </div>
-        <div className="col">
-          <p className="under-header-text">
-            Showing {totalCount} games in the database.
-          </p>
-
-          <GamesTable
-            handleRecord={this.handleRecord}
-            sortColumn={sortColumn}
-            games={games}
-            onLike={this.handleLike}
-            onDelete={this.handleDelete}
-            onSort={this.handleSort}
-            onComplete={this.handleComplete}
-          />
-
-          <Pagination
-            onPageChange={this.handlePageChange}
-            itemsCount={totalCount}
-            pageSize={pageSize}
-            currentPage={currentPage}
-          />
-        </div>
-      </div>
-    );
-  }
 }
 
 export default Games;
--- src/services/fakeGameService.js
+++ src/services/fakeGameService.js
@@ -1,178 +1,205 @@
-let games = [
-  {
-    gameplayCompletionist: 91,
-    gameplayMain: 35,
-    gameplayMainExtra: 62,
-    id: "36936",
-    imageUrl: "https://howlongtobeat.com/gameimages/36936_Nioh.jpg",
-    name: "Nioh",
-    liked: true,
-    completed: false,
-    timeSpent: 3,
-    get remainingTime() {
-      return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 34.5,
-    gameplayMain: 11.5,
-    gameplayMainExtra: 82.5,
-    id: "62935",
-    imageUrl: "https://howlongtobeat.com/gameimages/62935_The_Outer_Worlds.jpg",
-    name: "The Outer Worlds",
-    liked: true,
-    completed: false,
-    timeSpent: 0,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 11,
-    gameplayMain: 5.5,
-    gameplayMainExtra: 10.5,
-    id: "60145",
-    imageUrl:
-      "https://howlongtobeat.com/gameimages/50087_Nioh_-_Bloodsheds_End_DLC.jpg",
-    name: "Nioh - Bloodshed's End DLC",
-    liked: false,
-    completed: true,
-    timeSpent: 3,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 9.5,
-    gameplayMain: 3,
-    gameplayMainExtra: 5,
-    id: "7230",
-    imageUrl:
-      "https://howlongtobeat.com/gameimages/256px-Portal_standalonebox.jpg",
-    name: "Portal",
-    liked: false,
-    timeSpent: 0,
-    completed: false,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 20.5,
-    gameplayMain: 11.5,
-    gameplayMainExtra: 15.5,
-    id: "10469",
-    imageUrl: "https://howlongtobeat.com/gameimages/256px-Tr2011cover.jpg",
-    name: "Tomb Raider (2013)",
-    liked: false,
-    completed: false,
-    timeSpent: 0,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 35,
-    gameplayMain: 11.5,
-    gameplayMainExtra: 18,
-    id: "2708",
-    imageUrl: "https://howlongtobeat.com/gameimages/doom_2016.jpg",
-    name: "Doom (2016)",
-    liked: false,
-    completed: false,
-    timeSpent: 0,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 155,
-    gameplayMain: 46.5,
-    gameplayMainExtra: 75.5,
-    id: "27100",
-    imageUrl:
-      "https://howlongtobeat.com/gameimages/46360_Nioh_-_Dragon_of_the_north_DLC.jpg",
-    name: "Red Dead Redemption 2",
-    liked: false,
-    completed: false,
-    timeSpent: 3,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 22.5,
-    gameplayMain: 9,
-    gameplayMainExtra: 11.5,
-    id: "20737",
-    imageUrl: "https://howlongtobeat.com/gameimages/Metro2033Redux_292x136.jpg",
-    name: "Metro 2033 Redux",
-    liked: false,
-    completed: false,
-    timeSpent: 0,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 50.5,
-    gameplayMain: 20.5,
-    gameplayMainExtra: 31,
-    id: "38050",
-    imageUrl:
-      "https://howlongtobeat.com/gameimages/46360_Nioh_-_Dragon_of_the_north_DLC.jpg",
-    name: "God of War (2018)",
+import axios from 'axios';
 
-    liked: false,
-    completed: false,
-    timeSpent: 0,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  },
-  {
-    gameplayCompletionist: 51.5,
-    gameplayMain: 24,
-    gameplayMainExtra: 36.5,
-    id: "26286",
-    imageUrl:
-      "https://howlongtobeat.com/gameimages/Hollow_Knight_collection_branding.jpg",
-    name: "Hollow Knight",
 
-    liked: false,
-    completed: false,
-    timeSpent: 0,
-    get remainingTime() {
-        return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
-    }
-  }
-];
 
-export function getGames() {
-  return games;
+//
+//
+// let games = [
+//   {
+//     gameplayCompletionist: 91,
+//     gameplayMain: 35,
+//     gameplayMainExtra: 62,
+//     id: "36936",
+//     imageUrl: "https://howlongtobeat.com/gameimages/36936_Nioh.jpg",
+//     name: "Nioh",
+//     liked: true,
+//     completed: false,
+//     timeSpent: 3,
+//     get remainingTime() {
+//       return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 34.5,
+//     gameplayMain: 11.5,
+//     gameplayMainExtra: 82.5,
+//     id: "62935",
+//     imageUrl: "https://howlongtobeat.com/gameimages/62935_The_Outer_Worlds.jpg",
+//     name: "The Outer Worlds",
+//     liked: true,
+//     completed: false,
+//     timeSpent: 0,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 11,
+//     gameplayMain: 5.5,
+//     gameplayMainExtra: 10.5,
+//     id: "60145",
+//     imageUrl:
+//       "https://howlongtobeat.com/gameimages/50087_Nioh_-_Bloodsheds_End_DLC.jpg",
+//     name: "Nioh - Bloodshed's End DLC",
+//     liked: false,
+//     completed: true,
+//     timeSpent: 3,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 9.5,
+//     gameplayMain: 3,
+//     gameplayMainExtra: 5,
+//     id: "7230",
+//     imageUrl:
+//       "https://howlongtobeat.com/gameimages/256px-Portal_standalonebox.jpg",
+//     name: "Portal",
+//     liked: false,
+//     timeSpent: 0,
+//     completed: false,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 20.5,
+//     gameplayMain: 11.5,
+//     gameplayMainExtra: 15.5,
+//     id: "10469",
+//     imageUrl: "https://howlongtobeat.com/gameimages/256px-Tr2011cover.jpg",
+//     name: "Tomb Raider (2013)",
+//     liked: false,
+//     completed: false,
+//     timeSpent: 0,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 35,
+//     gameplayMain: 11.5,
+//     gameplayMainExtra: 18,
+//     id: "2708",
+//     imageUrl: "https://howlongtobeat.com/gameimages/doom_2016.jpg",
+//     name: "Doom (2016)",
+//     liked: false,
+//     completed: false,
+//     timeSpent: 0,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 155,
+//     gameplayMain: 46.5,
+//     gameplayMainExtra: 75.5,
+//     id: "27100",
+//     imageUrl:
+//       "https://howlongtobeat.com/gameimages/46360_Nioh_-_Dragon_of_the_north_DLC.jpg",
+//     name: "Red Dead Redemption 2",
+//     liked: false,
+//     completed: false,
+//     timeSpent: 3,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 22.5,
+//     gameplayMain: 9,
+//     gameplayMainExtra: 11.5,
+//     id: "20737",
+//     imageUrl: "https://howlongtobeat.com/gameimages/Metro2033Redux_292x136.jpg",
+//     name: "Metro 2033 Redux",
+//     liked: false,
+//     completed: false,
+//     timeSpent: 0,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 50.5,
+//     gameplayMain: 20.5,
+//     gameplayMainExtra: 31,
+//     id: "38050",
+//     imageUrl:
+//       "https://howlongtobeat.com/gameimages/46360_Nioh_-_Dragon_of_the_north_DLC.jpg",
+//     name: "God of War (2018)",
+//
+//     liked: false,
+//     completed: false,
+//     timeSpent: 0,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   },
+//   {
+//     gameplayCompletionist: 51.5,
+//     gameplayMain: 24,
+//     gameplayMainExtra: 36.5,
+//     id: "26286",
+//     imageUrl:
+//       "https://howlongtobeat.com/gameimages/Hollow_Knight_collection_branding.jpg",
+//     name: "Hollow Knight",
+//
+//     liked: false,
+//     completed: false,
+//     timeSpent: 0,
+//     get remainingTime() {
+//         return this.gameplayMain - this.timeSpent > 0 ? this.gameplayMain - this.timeSpent : 0;
+//     }
+//   }
+// ];
+export function getGames(userId) {
+    return axios.get(`http://localhost:8080/${userId}/games`);
 }
 
 export function getGame(id) {
-  return games.find(g => g.id === id);
+    // return games.find(g => g.id === id);
 }
 
 export function saveGame(game) {
-  game.timeSpent = 0;
-  const newGames = [...games, game];
-  games = newGames;
+
+    console.log(game);
+
+    let gameToSave = {
+        "UserId" : 2,
+        "id" : game["id"],
+        "imageUrl" : game["imageUrl"],
+        "name" : game["name"],
+        "gameplayCompletionist" : game["gameplayCompletionist"],
+        "gameplayMain" : game["gameplayMain"],
+        "gameplayMainExtra" : game["gameplayMainExtra"],
+    }
+
+    console.log(gameToSave)
+
+    axios.post("http://localhost:8080/games/save", gameToSave)
+        .then(response => {
+            if (response.status === 200) {
+                console.log(game);
+            } else {
+                throw new Error("Error");
+            }
+        }).catch(error => {
+        console.log(error)
+    });
 }
 
 export function updatePlayTime(game, time) {
-  time = time * 10000;
-  let hour = Math.floor(time / 1000 / 60 / 60);
-  let gameToChange = games.find(g => g.id === game.id);
-  console.log(hour);
-
-  gameToChange.timeSpent += hour;
+    // time = time * 10000;
+    // let hour = Math.floor(time / 1000 / 60 / 60);
+    // let gameToChange = games.find(g => g.id === game.id);
+    // console.log(hour);
+    //
+    // gameToChange.timeSpent += hour;
 }
 
 export function deleteGame(id) {
-  let gameInDb = games.find(g => g.id === id);
-  games.splice(games.indexOf(gameInDb), 1);
-  return gameInDb;
+    // let gameInDb = games.find(g => g.id === id);
+    // games.splice(games.indexOf(gameInDb), 1);
+    // return gameInDb;
 }
